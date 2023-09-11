8a8527c3e33f38e41da87af2fccc9979e811755d
André Elias <andre.carvalho@base2.com.br>
2023-09-11 16:06:48 -0300

    adds results and taunts

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -10,14 +10,13 @@ def read_user_play():
     parser.add_argument(
         "play", nargs='?', type=str, help="enter option 'rock', 'paper' or 'scissors'"
     )
-    """
     parser.add_argument(
         "-p",
         "--player",
         type=str,
-        help="sets a name for the human player"
+        default="User",
+        help="sets a custom name for the human player"
     )
-    """
     return parser.parse_args()
 
 def play_validation(play):
@@ -36,7 +35,7 @@ def user_play(play):
 def computer_play():
     return random.randint(0,2)
 
-def print_hand(option, player_name="User"):
+def print_hand(option, player_name):
     # Rock
     rock = """
         _______
@@ -75,7 +74,37 @@ def print_hand(option, player_name="User"):
         print(f"{player_name} has chosen: {POSSIBLE_PLAYS[option]}\n{scissors}")
 
 
-def print_result(a, b):
+def print_result(user_play, computer_play, user_name):
+    computer_taunts = [
+        f"{user_name} has lost! All hail the mighty computer!",
+        f"Impressive, computer! {user_name} can't keep up with your genius!",
+        f"The computer reigns supreme! {user_name}, try harder next time!",
+        f"Machine power! {user_name}, you're no match for our AI.",
+        f"{user_name}, you've met your match. The computer is unbeatable!",
+        f"Resistance is futile, {user_name}. The computer is here to conquer!"
+    ]
+    human_taunts = [
+        f"{user_name} has won! AI will never beat flesh and bones!",
+        f"{user_name}, you're on fire! Show that computer who's boss!",
+        f"Is that all you've got, computer? {user_name} is unstoppable!",
+        f"Computer, you're going down! {user_name} is here to dominate!",
+        f"AI, better luck next time! {user_name} is the real champion!",
+        f"Don't underestimate {user_name}, computer. They've got the skills to pay the bills!"
+    ]
+    if user_play == computer_play:
+        return f"Bananas, it's a draw!"
+    if user_play == 0 and computer_play == 1:
+        return f"{computer_taunts[random.randint(0,5)]}"
+    if user_play == 0 and computer_play == 2:
+        return f"{human_taunts[random.randint(0,5)]}"
+    if user_play == 1 and computer_play == 0:
+        return f"{human_taunts[random.randint(0,5)]}"
+    if user_play == 1 and computer_play == 2:
+        return f"{computer_taunts[random.randint(0,5)]}"
+    if user_play == 2 and computer_play == 0:
+        return f"{computer_taunts[random.randint(0,5)]}"
+    if user_play == 2 and computer_play == 1:
+        return f"{human_taunts[random.randint(0,5)]}"
     ...
 def print_error():
     return "The given input isn't valid.\nPlease be sure to enter 'rock', 'paper' or 'scissors'."
@@ -86,8 +115,9 @@ if __name__ == '__main__':
     if play_validation(user_args.play):
         user_choice = user_play(user_args.play)
         computer_choice = computer_play()
-        print_hand(user_choice)  # , user_args.player)
+        print_hand(user_choice, user_args.player)
         print_hand(computer_choice, "Computer")
+        print(print_result(user_choice, computer_choice, user_args.player))
     else:
         print(print_error())
 
