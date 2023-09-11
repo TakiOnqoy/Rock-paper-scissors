9e0a284653552c385adaba4c0c1553946dc92c6e
André Elias <andre.carvalho@base2.com.br>
2023-09-11 15:25:18 -0300

    versao funcional, play = lista

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,8 @@
 import argparse
 import random
 
+POSSIBLE_PLAYS = ['ROCK', 'PAPER', 'SCISSORS']
+
 def read_user_play():
     parser = argparse.ArgumentParser(
         description="Allows the player to play 'Rock, paper and scissors' on the terminal"
@@ -12,29 +14,29 @@ def read_user_play():
     parser.add_argument(
         "-p",
         "--player",
-        action="store_true",
-        help=""
+        type=str,
+        help="sets a name for the human player"
     )
     """
     return parser.parse_args()
 
-def user_play(play):
-    play_is_valid = False
-    possible_plays = ['ROCK', 'PAPER', 'SCISSORS']
+def play_validation(play):
     play[0] = play[0].upper()
-    for i in range(len(possible_plays)):
-        if play[0] == possible_plays[i]:
-            play_is_valid = True
-            break
-    if play_is_valid:
-        return print_hand(i)
-    else:
-        return print_hand(-1)
+    for option in POSSIBLE_PLAYS:
+        if play[0] == option:
+            return True
+    return False
+
+def user_play(play):
+    play[0] = play[0].upper()
+    for i in range(len(POSSIBLE_PLAYS)):
+        if play[0] == POSSIBLE_PLAYS[i]:
+            return i
 
 def computer_play():
-    return print_hand(random.randint(0,2))
+    return random.randint(0,2)
 
-def print_hand(play):
+def print_hand(option, player_name="User"):
     # Rock
     rock = """
         _______
@@ -65,19 +67,27 @@ def print_hand(play):
     ---.__(___)
     """
 
-    if play == 0:
-        print(f"Player has chosen: ROCK\n{rock}")
-    elif play == 1:
-        print(f"Player has chosen: PAPER\n{paper}")
-    elif play == 2:
-        print(f"Player has chosen: SCISSORS\n{scissors}")
+    if option == 0:
+        print(f"{player_name} has chosen: {POSSIBLE_PLAYS[option]}\n{rock}")
+    elif option == 1:
+        print(f"{player_name} has chosen: {POSSIBLE_PLAYS[option]}\n{paper}")
     else:
-        print("The input entered isn't valid.\nPlease be sure to enter 'rock', 'paper' or 'scissors'.")
+        print(f"{player_name} has chosen: {POSSIBLE_PLAYS[option]}\n{scissors}")
 
+
+def print_result(a, b):
+    ...
+def print_error():
+    return "The given input isn't valid.\nPlease be sure to enter 'rock', 'paper' or 'scissors'."
 
 
 if __name__ == '__main__':
     user_args = read_user_play()
-    user_play(user_args.play)
-    computer_play()
+    if play_validation(user_args.play):
+        user_choice = user_play(user_args.play)
+        computer_choice = computer_play()
+        print_hand(user_choice)  # , user_args.player)
+        print_hand(computer_choice, "Computer")
+    else:
+        print(print_error())
 
